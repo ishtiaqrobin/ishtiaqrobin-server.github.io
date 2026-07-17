6dbd60ab456c0454c4c95cc38c086d3ec4574325
Ishtiaq Robin <robin.pass36@gmail.com>
2026-07-17 23:37:50 +0600

    update review validation for max character is 500 and update user profile upload into the cloudinary

--- src/app/modules/review/review.validation.ts
+++ src/app/modules/review/review.validation.ts
@@ -3,21 +3,21 @@ import { z } from "zod";
 const createReviewZodSchema = z.object({
   position: z.string("position is required").min(1).max(50),
   companyName: z.string("company name is required").min(1).max(50),
-  comment: z.string("comment is required").min(2).max(500),
+  comment: z.string("comment is required").min(20).max(500),
 });
 
 // User can update position, company name, and comment
 const updateReviewZodSchema = z.object({
   position: z.string().min(1).max(50).optional(),
   companyName: z.string().min(1).max(50).optional(),
-  comment: z.string().min(2).max(500).optional(),
+  comment: z.string().min(20).max(500).optional(),
 });
 
 // Admin can also toggle isApproved and isPinned
 const adminUpdateReviewZodSchema = z.object({
   position: z.string().min(1).max(50).optional(),
   companyName: z.string().min(1).max(50).optional(),
-  comment: z.string().min(2).max(500).optional(),
+  comment: z.string().min(20).max(500).optional(),
 
   isApproved: z.boolean().optional(),
   isPinned: z.boolean().optional(),
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,9 @@
 import express, { Router } from "express";
 import auth, { UserRole } from "../../middlewares/auth";
 import { UserController } from "./user.controller";
-// import { multerUpload } from "../../config/multer.config";
+import { createMulterUpload } from "../../config/multer.config";
 
+const profileUpload = createMulterUpload("profiles");
 const router = express.Router();
 
 // Get current user profile
@@ -12,7 +13,7 @@ router.get("/me", auth(UserRole.USER, UserRole.ADMIN), UserController.getMe);
 router.put(
   "/profile",
   auth(UserRole.USER, UserRole.ADMIN),
-  // multerUpload.single("image"),
+  profileUpload.single("image"),
   UserController.updateProfile,
 );
 
